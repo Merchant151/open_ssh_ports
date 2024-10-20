c1ff05622023514998fdc6585f1849a38ad09fe4
Joshua "Mart" Miller <32748201+Merchant151@users.noreply.github.com>
2024-10-19 21:10:06 -0300

    Update sleepssh.sh stop fail2ban

--- bin/sleepssh.sh
+++ bin/sleepssh.sh
@@ -3,7 +3,7 @@ sudo systemctl stop ssh
 sudo systemctl disable ssh
 #instead of reseting ufw I should just remove my rules! 
 sudo ufw reset
-
+sudo systemctl stop fail2ban
 #status and info
 sudo systemctl status ssh
 sudo ufw status verbose
