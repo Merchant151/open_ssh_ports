ab5d420ceb2c136443e1224dbee1c58096f15b40
Joshua "Mart" Miller <32748201+Merchant151@users.noreply.github.com>
2024-10-19 21:09:01 -0300

    Update wakessh.sh with start fail2ban

--- bin/wakessh.sh
+++ bin/wakessh.sh
@@ -4,5 +4,6 @@ sudo ufw allow from 192.168.1.0/24 to any port 22
 sudo systemctl start ssh
 sudo ufw status verbose
 sudo systemctl status ssh
+sudo systemctl start fail2ban
 # greping out ipv4 addresses in ip (not currently sure how to grab my specific network consistently)  
 ip a |grep inet|grep -v inet6
