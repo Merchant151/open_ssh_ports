bdd18136f8628e687086933eede4d96d4cc36726
Merchant151 <jmiller32@ggc.edu>
2024-10-26 14:24:10 -0400

    logging to single log file problem with ps ownership

--- bin/sauron.sh
+++ bin/sauron.sh
@@ -1,10 +1,16 @@
+#!/bin/bash
 #My goal for this script is to create a standalone logger  that while running will send data to a local log and will also tail that log
 
 #There are multiple logs I would like to follow from the var/log 
 echo -n '<fail log>'
-tail -1 /var/log/fail2ban
+tail -1 /var/log/fail2ban.log
 echo -n '<auth log>' && tail -1 /var/log/auth.log
 echo -n '<ufw log>' && tail -1 /var/log/ufw.log
 
 #I will likely need to use a while loop on a read command to only update when changes happen and then tail the file with those changes
 
+tail -vf /var/log/fail2ban.log | awk 'BEGIN{p="fail2ban.log"} {print p,$0}' >> ../logs/sauron.log &
+tail -vf /var/log/auth.log | awk 'BEGIN{p="auth.log"} {print p,$0}' >> ../logs/sauron.log &
+tail -vf /var/log/ufw.log | awk 'BEGIN{p="ufw.log"} {print p,$0}' >> ../logs/sauron.log & 
+
+sleep 120
