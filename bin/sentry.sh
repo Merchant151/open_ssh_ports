af4176a3efd3587fedb6244fd5654ee0580a6a25
Merchant151 <joshntram@gmail.com>
2024-11-02 19:36:52 -0400

    sentry modes

--- bin/sentry.sh
+++ bin/sentry.sh
@@ -1,4 +1,35 @@
-#!bin/bash
+#!/bin/bash
+
+#my goal for this script is to be run every hour in cron mode just to check on ssh status 
+#Insuring ssh isn't randomly on.
+#this script will also have a separate mode for standard run. It will be activated when mySsh is turned on. 
+MODE='none'
+while getopts ":cs" option; do
+	case $option in 
+		c)
+			echo 'cron case'
+			MODE='cron'
+			;;
+		s)
+			echo 'standard case'
+			MODE='standard'
+			;;
+		esac
+	done
+
+if [ $MODE == 'cron' ]
+then 
+	echo "cron mode" 
+	exit
+elif [ $MODE == 'standard' ]
+then 
+	echo 'standard mode'
+	exit
+else
+	echo 'no mode selected'
+	exit	
+fi
+
 
 print_status
 
