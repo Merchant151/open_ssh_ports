8f13ecefeb5ef03ab29e58254986be0829f83084
Merchant151 <joshntram@gmail.com>
2024-11-06 09:13:12 -0500

    myssh comments

---
+++ bin/myssh.sh
@@ -0,0 +1,14 @@
+#shibang 
+
+#bin options (for now just time in hours)
+
+#catch term out and run stop script 
+
+#convert time to hours 
+
+#run sauron
+#run sentry
+#run start script 
+
+#sleep for number of hours 
+#run end script 
