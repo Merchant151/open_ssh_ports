edcff47dab36d0622257cbac7046569aacd6045b
Merchant151 <joshntram@gmail.com>
2024-11-08 19:42:52 -0500

    options for myssh

--- bin/myssh.sh
+++ bin/myssh.sh
@@ -1,6 +1,22 @@
 #shibang 
 
 #bin options (for now just time in hours)
+while getopts ":h" option; do
+	case $option in 
+		h)
+			usage
+			exit;;
+		esac
+	done
+
+#help option
+usasge(){
+	#The purpose of this script
+	#options of this script include
+	#-h show script usage
+	# others 
+	
+}
 
 #catch term out and run stop script
 murderProcess(){
