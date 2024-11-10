fd2449511dd9af86bf63f6cb31a331dbc309c0a4
Merchant151 <joshntram@gmail.com>
2024-11-10 12:41:31 -0500

    learned how to read optargs

--- bin/myssh.sh
+++ bin/myssh.sh
@@ -1,11 +1,14 @@
 #shibang 
-
+TIME=1
 #bin options (for now just time in hours)
-while getopts ":h" option; do
+while getopts ":ht:" option; do
 	case $option in 
 		h)
 			usage
 			exit;;
+		t)
+			echo "option time set to $OPTARG hours" 
+			TIME=$OPTARG
 		esac
 	done
 
@@ -15,7 +18,7 @@ usasge(){
 	#options of this script include
 	#-h show script usage
 	# others 
-	
+	echo 'none'	
 }
 
 #catch term out and run stop script
@@ -27,7 +30,8 @@ exit
 trap murderProcess SIGINT SIGTERM
 
 #convert time to hours 
-
+#check if time is an int or throw error 
+echo running for $TIME hours
 #run sauron
 #run sentry
 #run start script 
