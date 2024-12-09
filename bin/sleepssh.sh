84f3993dbe4098cd09e753caa90145872d5d2cc6
Merchant151 <joshntram@gmail.com>
2024-12-08 23:58:21 -0500

    removed sudo from sleepsh

--- bin/sleepssh.sh
+++ bin/sleepssh.sh
@@ -1,12 +1,12 @@
 #stoping and reseting firewall
-sudo systemctl stop ssh
-sudo systemctl disable ssh
+systemctl stop ssh
+systemctl disable ssh
 #sudo ufw reset
-sudo ufw delete allow from 192.168.1.0/24 to any port 443 
-sudo ufw delete allow from 192.168.1.0/24 to any port 22 
-sudo systemctl stop fail2ban
+ufw delete allow from 192.168.1.0/24 to any port 443 
+ufw delete allow from 192.168.1.0/24 to any port 22 
+systemctl stop fail2ban
 #status and info
-sudo systemctl status ssh
-sudo ufw status verbose
+systemctl status ssh
+ufw status verbose
 ps aux | grep ssh|grep -v 'grep ssh' 
 
