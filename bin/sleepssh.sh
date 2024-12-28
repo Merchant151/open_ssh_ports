cfc3c478dfc70746d1eaba5f65584fba3e9ec0bc
Merchant151 <jmiller32@ggc.edu>
2024-12-27 20:58:19 -0500

    added full logging to sleepssh script

--- bin/sleepssh.sh
+++ bin/sleepssh.sh
@@ -1,12 +1,21 @@
 #stoping and reseting firewall
+D=$(date)
+LOGFILE=$(dirname "$0")../logs/sleepssh.log
+echo "sleepssh was activated at $D"
 systemctl stop ssh
-systemctl disable ssh
+echo "RESULT OF STOP SSH is $?" >> $LOGFILE
+systemctl disable ssh >> $LOGFILE
+echo "RESULT OF DISABLE SSH is $?" >> $LOGFILE
 #sudo ufw reset
-ufw delete allow from 192.168.1.0/24 to any port 443 
+ufw delete allow from 192.168.1.0/24 to any port 443
+echo "RESULT OF UFW DELETE ALLOW to 443 is $?" >> $LOGFILE
 ufw delete allow from 192.168.1.0/24 to any port 22 
+echo "RESULT OF UFW DELETE ALLOW to 22 is $?" >> $LOGFILE
 systemctl stop fail2ban
 #status and info
-systemctl status ssh
-ufw status verbose
+systemctl status ssh >> $LOGFILE
+ufw status verbose >> $LOGFILE
 ps aux | grep ssh|grep -v 'grep ssh' 
-
+D=$(date)
+echo "sleepssh completed run at $D" >> $LOGFILE
+echo "=============================================" >> $LOGFILE
